df1911876a3819b95c62e2de4d61f2555d87f556
Xigndong Bian <xingdong.bian@earlham.ac.uk>
2018-05-30 16:28:02 +0100

    add new query

--- apisearch-unified.php
+++ apisearch-unified.php
@@ -9,9 +9,27 @@ if(isset($_GET['query'])) {
 
 $id = (int)$query;
 
-$query1 = "SELECT * FROM plant left join storeref on plant.idPlant=storeref.idPlant WHERE lower(plant.AccessionName) LIKE lower('%$query%') OR plant.idPlant=$id";
+//$query1 = "SELECT * FROM plant left join storeref on plant.idPlant=storeref.idPlant WHERE lower(plant.AccessionName) LIKE lower('%$query%') OR plant.idPlant=$id";
 
-$query2 = "SELECT * FROM storeref left join plant on storeref.idPlant=plant.idPlant WHERE lower(storeref.StoreCode) LIKE lower('%$query%')";
+$query2 = "SELECT idPlant FROM storeref left join plant on storeref.idPlant=plant.idPlant WHERE lower(storeref.StoreCode) LIKE lower('%$query%')";
+
+$query1="SELECT plant.idPlant, plant.SubCollection, plant.AccessionName, StoreCode, Genus, Species, SubTaxa, Pedigree, GROUP_CONCAT(Synonym) as Synonyms, 
+  donor.Name as DonorName, breeder.Name as BreederName, SampStatDesc, Country, SowSeason, AccYear, CollSite, PhenotypeParameter, PhenotypeValue, PhenotypeDescribedBy
+						FROM `plant`  
+						JOIN `taxon` ON plant.idTaxon=taxon.idTaxon
+						LEFT JOIN `pedigree` ON plant.idPedigree=pedigree.idPedigree
+						LEFT JOIN `exped` ON plant.idPlant=exped.idPlant
+						LEFT JOIN `synonym` ON plant.idPlant=synonym.idPlant
+						LEFT JOIN `somebody` as donor ON plant.idDonor=donor.idSomebody
+						LEFT JOIN `somebody` as breeder ON plant.idBreeder = breeder.idSomebody
+						LEFT JOIN `codesampstat` ON plant.SampStat =codesampstat.SampStat
+						LEFT JOIN `codecountry` ON plant.OriginCountry =codecountry.CountryCode
+						LEFT JOIN `storeref` ON plant.idPlant=storeref.idPlant
+						LEFT JOIN `phenotype` ON plant.idPlant=phenotype.idPlant
+						WHERE (plant.idPlant=$id) OR (lower(plant.AccessionName) LIKE lower('%$query%'))
+						GROUP BY plant.idPlant";
+
+
 
 $rows1 = array();
 $rows2 = array();
@@ -29,7 +47,7 @@ if ($result2 = $dbcnx->query($query2)) {
     $result2->close();
 }
 $rows = $rows1 + $rows2;
-print json_encode($rows);
+print json_encode($rows1);
 
 $dbcnx->close();
 ?>
